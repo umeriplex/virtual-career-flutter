39602a2cec44634453b143cfa7b264c29998f2d0
umertaptap <umer.iftikhar@taptaptechnologies.com>
2026-02-02 03:08:17 +0500

    final fixes

--- lib/core/constant/app_constants.dart
+++ lib/core/constant/app_constants.dart
@@ -1,4 +1,4 @@
 class AppConstants {
-  static const geminiKey = "AIzaSyCz0SGfiVhN8Yu0fX_JdKNxVMZ0SPgEJ4s";
+  static const geminiKey = "----";
   static const String placeHolderImage = 'https://firstbenefits.org/wp-content/uploads/2017/10/placeholder-1024x1024.png';
 }
